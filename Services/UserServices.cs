c3eb4a94e16aac0fffdc147d5977dbc737d49889
rob2fresh4this <grijrob07@gmail.com>
2025-03-24 11:58:27 -0700

    Add DataContext dependency to UserServices for improved data access

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -1,8 +1,17 @@
 
+using Study_Buddys_Backend.Context;
+
 namespace Study_Buddys_Backend.Services
 {
     public class UserServices
     {
+        private readonly DataContext _dataContext;
+
+        public UserServices(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
         
     }
 }
