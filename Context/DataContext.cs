991db6d5cc37f71451652ffd1792ad8681dd7ef1
rob2fresh4this <grijrob07@gmail.com>
2025-03-24 11:46:49 -0700

    Add DataContext class for Entity Framework Core integration

---
+++ Context/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Study_Buddys_Backend.Models;
+
+namespace Study_Buddys_Backend.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<UserModels> Users { get; set; }
+    }
+}
