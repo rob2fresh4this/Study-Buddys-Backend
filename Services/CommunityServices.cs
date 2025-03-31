3642a45c2aea858fd52de715467a16b0e58c60c7
rob2fresh4this <grijrob07@gmail.com>
2025-03-31 16:05:26 -0700

    Add dependency injection for DataContext in CommunityServices

--- Services/CommunityServices.cs
+++ Services/CommunityServices.cs
@@ -1,9 +1,16 @@
-
+using Study_Buddys_Backend.Context;
 
 namespace Study_Buddys_Backend.Services
 {
     public class CommunityServices
     {
+        private readonly DataContext _dataContext;
+
+        public CommunityServices(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
         
     }
 }
